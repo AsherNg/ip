4074be2402db1bfb357edfb1038520a72f520c63
Asher Ng <asherng.sg@gmail.com>
2026-08-20 15:35:29 +0800

    Add greeting

--- src/main/java/CharlieK.java
+++ src/main/java/CharlieK.java
@@ -1,10 +1,17 @@
 public class CharlieK {
     public static void main(String[] args) {
         String banner = "  ____ _                _ _      _  __\n"
-                + " / ___| |__   __ _ _ __| (_) ___| |/ /\n"
-                + "| |   | '_ \\ / _` | '__| | |/ _ \\ ' / \n"
-                + "| |___| | | | (_| | |  | | |  __/ . \\ \n"
-                + " \\____|_| |_|\\__,_|_|  |_|_|\\___|_|\\_\\\n";
-        System.out.println(banner);
+                        + " / ___| |__   __ _ _ __| (_) ___| |/ /\n"
+                        + "| |   | '_ \\ / _` | '__| | |/ _ \\ ' / \n"
+                        + "| |___| | | | (_| | |  | | |  __/ . \\ \n"
+                        + " \\____|_| |_|\\__,_|_|  |_|_|\\___|_|\\_\\\n";
+                        
+        System.out.println("____________________________________________________________");
+        System.out.print(banner);
+        System.out.println("Hello! I'm CharlieK.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
