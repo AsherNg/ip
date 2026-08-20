50cb40557ffc53bd9b66630e7f2ff67347a523c6
Asher Ng <asherng.sg@gmail.com>
2026-08-20 15:50:45 +0800

    Add support for list

--- src/main/java/CharlieK.java
+++ src/main/java/CharlieK.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
  */
 public class CharlieK {
     private static final String LINE = "____________________________________________________________";
+    private static final int MAX_TASKS = 100;
+
+    /**
+     * Stores the tasks entered during this run of the program.
+     * The tasks are intentionally kept in memory only, as required.
+     */
+    private static final String[] tasks = new String[MAX_TASKS];
+
+    /** Number of tasks currently stored. */
+    private static int taskCount = 0;
 
     public static void main(String[] args) {
         String banner = "  ____ _                _ _      _  __\n"
@@ -30,8 +40,32 @@ public class CharlieK {
                 break;
             }
 
-            System.out.println("     " + command);
+            if (command.equals("list")) {
+                printTasks();
+            } else {
+                addTask(command);
+                System.out.println("     added: " + command);
+            }
             System.out.println(LINE);
+        }
+    }
+
+    /**
+     * Adds a task to the in-memory task list.
+     *
+     * @param task the text entered by the user
+     */
+    private static void addTask(String task) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount] = task;
+            taskCount++;
+        }
+    }
+
+    /** Displays all stored tasks in the order they were entered. */
+    private static void printTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("     " + (i + 1) + ". " + tasks[i]);
         }
     }
 }
