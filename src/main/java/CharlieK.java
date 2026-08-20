ef4517499a5d074d40db3139047c0e9766a1da54
Asher Ng <asherng.sg@gmail.com>
2026-08-20 15:45:04 +0800

    Add support for echo

--- src/main/java/CharlieK.java
+++ src/main/java/CharlieK.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
+
+/**
+ * Runs the CharlieK command-line chatbot.
+ */
 public class CharlieK {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = "  ____ _                _ _      _  __\n"
                         + " / ___| |__   __ _ _ __| (_) ___| |/ /\n"
@@ -6,12 +13,25 @@ public class CharlieK {
                         + "| |___| | | | (_| | |  | | |  __/ . \\ \n"
                         + " \\____|_| |_|\\__,_|_|  |_|_|\\___|_|\\_\\\n";
                         
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
         System.out.print(banner);
         System.out.println("Hello! I'm CharlieK.");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(LINE);
+
+            if (command.equals("bye")) {
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+
+            System.out.println("     " + command);
+            System.out.println(LINE);
+        }
     }
 }
